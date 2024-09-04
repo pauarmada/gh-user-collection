8dfd2d4a2aca8395cf5481b1a6bd26117d811a1d
Paulus Armada <pauarmada@gmail.com>
2024-09-04 20:52:20 +0900

    Create base folder structure and placeholder content
    * Created project from xcode
    * Added gitignore

---
+++ GhUserCollection/GhUserCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhUserCollectionApp.swift
+//  GhUserCollection
+//
+//  Created by Paulus Armada on 2024/09/05.
+//
+
+import SwiftUI
+
+@main
+struct GhUserCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
