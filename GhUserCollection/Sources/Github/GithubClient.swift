14c29432a5e4dead083d557d2870132d01e3dc17
Paulus Armada <pauarmada@gmail.com>
2024-09-06 00:09:26 +0900

    Add github rest api access and models
    * Integrated Alamofire for request and response handling
    * Parsed mininum required information from requests

---
+++ GhUserCollection/Sources/Github/GithubClient.swift
@@ -0,0 +1,61 @@
+//
+//  GithubClient.swift
+//  GhUserCollection
+//
+//  Created by Paulus Armada on 2024/09/05.
+//
+
+import Alamofire
+import ArkanaKeys
+import Foundation
+
+class GithubClient {
+    private let baseUrl: String
+    private let headers: HTTPHeaders
+    
+    // Use a custom decoder to take care of the snake case
+    let decoder: JSONDecoder = {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }()
+    
+    init(accessToken: String = ArkanaKeys.Global().githubToken) {
+        self.baseUrl = "https://api.github.com"
+        // Attach the authorization token to the headers
+        self.headers = HTTPHeaders(
+            [
+                HTTPHeader(name: "Authorization", value: "Bearer \(accessToken)"),
+                HTTPHeader(name: "Accept", value: "application/vnd.github+json"),
+            ]
+        )
+    }
+    
+    // https://docs.github.com/en/rest/users/users?apiVersion=2022-11-28#list-users
+    func getUsers(since: Int64 = 0, perPage: Int = 100) async throws -> [GithubUser] {
+        try await get("\(baseUrl)/users?per_page=\(perPage)&since=\(since)")
+    }
+    
+    // https://docs.github.com/en/rest/users/users?apiVersion=2022-11-28#get-a-user-using-their-id
+    func getUserInfo(login: String) async throws -> GithubUserInfo {
+        try await get("\(baseUrl)/users/\(login)")
+    }
+    
+    // https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repositories-for-a-user
+    func getRepositories(login: String) async throws -> [GithubRepository] {
+        try await get("\(baseUrl)/users/\(login)/repos")
+    }
+
+    private func get<T>(_ convertible: URLConvertible) async throws -> T where T: Decodable {
+        try await AF
+            .request(
+                convertible,
+                headers: headers
+            )
+            .serializingResponse(
+                using: DecodableResponseSerializer<T>(decoder: decoder)
+            )
+            .value
+    }
+}
+
---
+++ GhUserCollectionTests/Github/GithubModelTests.swift
@@ -0,0 +1,108 @@
+//
+//  GithubModelTests.swift
+//  GhUserCollectionTests
+//
+//  Created by Paulus Armada on 2024/09/05.
+//
+
+import XCTest
+@testable import GhUserCollection
+
+final class GithubModelsTests: XCTestCase {
+    
+    private let decoder = GithubClient().decoder
+    
+    private func response(jsonString: String) -> Data {
+        jsonString.data(using: .utf8)!
+    }
+    
+    func test_decode_users() throws {
+        let jsonString = """
+        [
+          {
+            "login": "octocat",
+            "id": 1,
+            "avatar_url": "https://github.com/images/error/octocat_happy.gif"
+          },
+          {
+            "login": "octocat2",
+            "id": 2,
+            "avatar_url": "https://github.com/images/error/octocat_happy2.gif"
+          }
+        ]
+        """
+        let users = try decoder.decode([GithubUser].self, from: response(jsonString: jsonString))
+        XCTAssert(users.count == 2)
+        
+        let user1 = users.first!
+        XCTAssertEqual(user1.id, 1)
+        XCTAssertEqual(user1.login, "octocat")
+        XCTAssertEqual(user1.avatarUrl, URL(string: "https://github.com/images/error/octocat_happy.gif"))
+        
+        let user2 = users.last!
+        XCTAssertEqual(user2.id, 2)
+        XCTAssertEqual(user2.login, "octocat2")
+        XCTAssertEqual(user2.avatarUrl, URL(string: "https://github.com/images/error/octocat_happy2.gif"))
+    }
+    
+    func test_decode_userInfo() throws {
+        let jsonString = """
+        {
+          "id": 1,
+          "name": "monalisa octocat",
+          "followers": 20,
+          "following": 0
+        }
+        """
+        let userInfo = try decoder.decode(GithubUserInfo.self, from: response(jsonString: jsonString))
+        
+        XCTAssertEqual(userInfo.id, 1)
+        XCTAssertEqual(userInfo.name, "monalisa octocat")
+        XCTAssertEqual(userInfo.followers, 20)
+        XCTAssertEqual(userInfo.following, 0)
+    }
+    
+    func test_decode_repositories() throws {
+        let jsonString = """
+        [
+          {
+            "id": 1296269,
+            "name": "min-fields",
+            "html_url": "https://github.com/octocat/Hello-World",
+            "description": null,
+            "fork": false,
+            "language": null,
+            "stargazers_count": 0,
+          },
+          {
+            "id": 1296270,
+            "name": "full-description",
+            "html_url": "https://github.com/octocat/Hello-World2",
+            "description": "This your first repo!",
+            "fork": true,
+            "language": "swift",
+            "stargazers_count": 80
+          }
+        ]
+        """
+        let repositories = try decoder.decode([GithubRepository].self, from: response(jsonString: jsonString))
+        
+        let repo1 = repositories.first!
+        XCTAssertEqual(repo1.id, 1296269)
+        XCTAssertEqual(repo1.name, "min-fields")
+        XCTAssertNil(repo1.description)
+        XCTAssertFalse(repo1.fork)
+        XCTAssertNil(repo1.language)
+        XCTAssertEqual(repo1.stargazersCount, 0)
+        XCTAssertEqual(repo1.htmlUrl, URL(string: "https://github.com/octocat/Hello-World"))
+        
+        let repo2 = repositories.last!
+        XCTAssertEqual(repo2.id, 1296270)
+        XCTAssertEqual(repo2.name, "full-description")
+        XCTAssertEqual(repo2.description, "This your first repo!")
+        XCTAssertTrue(repo2.fork)
+        XCTAssertEqual(repo2.language, "swift")
+        XCTAssertEqual(repo2.stargazersCount, 80)
+        XCTAssertEqual(repo2.htmlUrl, URL(string: "https://github.com/octocat/Hello-World2"))
+    }
+}
