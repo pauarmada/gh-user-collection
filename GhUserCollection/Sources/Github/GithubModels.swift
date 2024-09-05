faee1d2e6e38bd5a64d2c3161085aff9285d56a8
Paulus Armada <pauarmada@gmail.com>
2024-09-06 00:14:55 +0900

    Add github rest api access and models (#3)
    * Integrated Alamofire for request and response handling
    * Parsed mininum required information from requests

---
+++ GhUserCollection/Sources/Github/GithubModels.swift
@@ -0,0 +1,52 @@
+//
+//  GithubModels.swift
+//  GhUserCollection
+//
+//  Created by Paulus Armada on 2024/09/05.
+//
+
+import Foundation
+
+/**
+ Each row should include the following elements;
+　・User's avatar image
+　・Username
+ */
+struct GithubUser: Decodable, Equatable {
+    let id: Int64
+    let login: String
+    let avatarUrl: URL
+}
+
+/**
+ Detailed information at the top of the list.
+ ・Avatar image
+ ・Username
+ ・Full name
+ ・Number of followers
+ ・Number of followings
+ */
+struct GithubUserInfo: Decodable {
+    let id: Int64
+    let name: String
+    let followers: Int
+    let following: Int
+}
+
+/**
+ Below, display a list of the user's original repositories (non-forked), including the following details:
+ ・Repository name
+ ・Programming language used
+ ・Number of stars
+ ・Description
+ */
+// Assumption: For now that this is only the "language" field
+struct GithubRepository: Decodable, Equatable {
+    let id: Int64
+    let name: String
+    let description: String?
+    let fork: Bool
+    let language: String?
+    let stargazersCount: Int
+    let htmlUrl: URL
+}
---
+++ GhUserCollectionTests/Github/GithubClientTests.swift
@@ -0,0 +1,61 @@
+//
+//  GithubClientTests.swift
+//  GhUserCollectionTests
+//
+//  Created by Paulus Armada on 2024/09/05.
+//
+
+import XCTest
+@testable import GhUserCollection
+
+final class GithubClientTests: XCTestCase {
+    
+    private func client(customAccessToken: String? = nil) -> GithubClient {
+        if let customAccessToken {
+            GithubClient(accessToken: customAccessToken)
+        } else {
+            GithubClient()
+        }
+    }
+    
+    func test_invalidAccessToken() async {
+        do {
+            _ = try await client(customAccessToken: "this_dont_work").getUsers(perPage: 1)
+            XCTFail("Should not succeed")
+        } catch {
+            // No-Op
+        }
+    }
+    
+    func test_getUsers() async throws {
+        let client = client()
+        let users = try await client.getUsers(perPage: 1)
+        XCTAssert(users.count == 1)
+        
+        // Paging should work
+        let user = users.first!
+        let nextUsers = try await client.getUsers(since: user.id, perPage: 1)
+        XCTAssert(nextUsers.count == 1)
+        
+        XCTAssertNotEqual(users.first, nextUsers.first)
+    }
+    
+    func test_getUserInfo() async throws {
+        let client = client()
+        let users = try await client.getUsers(perPage: 2)
+        XCTAssert(users.count > 1)
+        
+        let user = users.first!
+        let userInfo = try await client.getUserInfo(login: user.login)
+        XCTAssertEqual(user.id, userInfo.id)
+    }
+    
+    func test_getRepositories() async throws {
+        let client = client()
+        let users = try await client.getUsers(perPage: 2)
+        XCTAssert(users.count > 1)
+        
+        let user = users.first!
+        _ = try await client.getRepositories(login: user.login)
+    }
+}
